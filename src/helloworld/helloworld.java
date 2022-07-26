23e4232245bea7d1ae8d28742e363f8afb40126b
Smanan2000 <soni.manan3150@gmail.com>
2022-07-27 01:41:49 +0530

    t1

---
+++ src/helloworld/helloworld.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+public class helloworld {
+		public static void main(String[] arg) {
+			
+			System.out.println("hello world");
+			
+		}
+		class test{
+			{
+			System.out.println("helloworld");
+			}
+			helloworld h1 = new helloworld();
+		}
+}
+
+
