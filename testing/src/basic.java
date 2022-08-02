81a70e47b7bc653ba10a20dbe06017a421c19554
Smanan2000 <soni.manan3150@gmail.com>
2022-08-02 08:03:01 +0530

    commit

---
+++ testing/src/basic.java
@@ -0,0 +1,29 @@
+/*
+ * 
+ * data types
+ * 
+ * type				class 			size		convert (bit or byte)
+ * 
+ * short			Short			1byte			8 bit
+ * byte				Byte 			1byte			8 bit
+ * char				Char			1byte			8 bit
+ * int				Integer 		4 byte			32 bit
+ * float			Float			4 byte			32 bit
+ * double			Double			8 byte			64 bit
+ * long				Long			8 byte			64 bit
+ * boolean			Boolean 		1 bit(true or false) 0.125 byte
+ * 
+ * 
+ */
+
+public class basic {
+	public static void main(String[] args){
+		System.out.println("java programming");
+		System.out.println("it is enjouing to learn");
+		int a = 10 , b = 20;
+		System.out.println("\nA=" + a + "\tb="+b+"\t"+(a+b));
+		System.out.println("\nA=" + a + "\tb="+b+"\t"+(a-b));
+		System.out.println("\nA=" + a + "\tb="+b+"\t"+(a/b));
+		System.out.println("\nA=" + a + "\tb="+b+"\t"+(a*b));
+	}
+}
