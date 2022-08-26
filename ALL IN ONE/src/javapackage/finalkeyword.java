d1a7b6825392e2bb206ebc32d02415192f3a9e64
Smanan2000 <soni.manan3150@gmail.com>
2022-08-26 21:45:16 +0530

    COMMIT

---
+++ ALL IN ONE/src/javapackage/finalkeyword.java
@@ -0,0 +1,25 @@
+package javapackage;
+
+//finalkeyword is if you declare variable as final then u cannot change and also need to assign compoulsory.
+
+//if declare method as final then method cannot be overriden.
+
+//if declare class as final then you cananot inherit that class.
+
+class final1{
+	final int a = 10;
+	
+	final void show()
+	{
+		System.out.println("value of a is"+a);
+	}
+}
+class final2 extends final1{
+	//
+}
+public class finalkeyword {
+	public static void main(String[] args) {
+	final2 f1 = new final2();
+	f1.show();
+}
+}
--- ALL IN ONE/src/javapackage/polymorphism.java
+++ ALL IN ONE/src/javapackage/polymorphism.java
@@ -1,4 +1,5 @@
 package javapackage;
+import java.util.*;
 
 
 /*
@@ -26,7 +27,7 @@ package javapackage;
  * 
  * 
  */
-
+/*
 
 class MethodOverloadingDemo{
 	
@@ -59,6 +60,127 @@ public class polymorphism {
 		System.out.println("Addition = "+mo.add(3,3,5));
 	}
 }
+
+*/
+
+//static keyword example
+/*
+public class polymorphism
+{
+	static int a;
+	
+	void getval()
+	{
+		Scanner sc  = new Scanner(System.in);
+		System.out.println("enter value of a");
+		a = sc.nextInt();
+	}
+	
+	void printval()
+	{
+		System.out.println("value of a is "+ a);
+	}		
+	
+	public static void main(String[] args) {
+		polymorphism p1 = new polymorphism();
+		polymorphism p2 = new polymorphism();
+		polymorphism p3 = new polymorphism();
+		
+		p1.getval();
+		p2.getval();
+		p3.getval();
+		
+		p1.printval();
+		p2.printval();
+		p3.printval();
+	}
+}
+*/
+
+//static method example
+/*
+public class  polymorphism{
+
+	static int a =10;
+	 static int b;
+	
+	static void meth(int x)
+	{
+		System.out.println("X = "+ x);
+		System.out.println("A = "+ a);
+		System.out.println("B = "+ b);
+	}
+	
+	{
+		System.out.println("Block 1 Called");
+	}
+	
+	 {
+		System.out.println("Static block Initialized");
+		b = a*4;
+	}
+	
+	{
+		System.out.println("Block 2 called");
+	}
+	
+	
+	public static void main(String[] args) {
+		polymorphism s1 = new polymorphism();
+		meth(12);
+	}
+}
+*/
+//final keyword
+class final1{
+	
+final int a = 100;
+
+final void show()
+{
+	
+	System.out.println("A = "+a);
+}
+}
+
+class final2  extends final1{
+
+//void show()
+//{
+//
+//}
+}
+
+
+public class polymorphism {
+
+public static void main(String[] args) {
+	final1 f1 = new final1();
+	f1.show();
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
