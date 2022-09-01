5429501267776054ce874d4631d7926ad2e6a499
Smanan2000 <soni.manan3150@gmail.com>
2022-09-01 22:25:00 +0530

    commit

---
+++ java topic/src/snippet/Snippet.java
@@ -0,0 +1,10 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		case '+':
+			System.out.println("no1 and no2 addition is"+(no1+no2));
+			break;
+	}
+}
+
---
+++ java topic/src/topic/method.java
@@ -0,0 +1,108 @@
+package topic;
+
+import java.util.Scanner;
+
+/*
+class teapost{
+	Scanner sc =new Scanner(System.in);
+	String s;
+	void getval()
+	{
+		System.out.println("entere your name");
+		s = sc.nextLine();
+		
+	}
+	void showval()
+	{
+		System.out.println("name is\t"+s);
+	}
+}
+public class method {
+	public static void main(String[] args) {
+		teapost t1 = new teapost();
+		t1.getval();
+		t1.showval();
+	}
+}
+*/
+
+/*
+class calc{
+	Scanner sc = new Scanner(System.in);
+	int ans,no1,no2;
+	
+	void getval()
+	{
+		System.out.println("welcome");
+		System.out.println("enter no 1");
+		no1 = sc.nextInt();
+		System.out.println("enter no 2");
+		no2 = sc.nextInt();
+		System.out.println("addition is\t"+(no1+no2));
+		System.out.println("addition is\t"+(no1-no2));
+		System.out.println("addition is\t"+(no1*no2));
+		System.out.println("addition is\t"+(no1/no2));
+	}
+	void show()
+	{
+		System.out.println("addition is\t"+(no1+no2));
+		System.out.println("addition is\t"+(no1-no2));
+		System.out.println("addition is\t"+(no1*no2));
+		System.out.println("addition is\t"+(no1/no2));
+	}
+}
+public class method{
+	public static void main(String[] args) {
+		calc c1 = new calc();
+		c1.getval();
+		c1.show();
+	}
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
