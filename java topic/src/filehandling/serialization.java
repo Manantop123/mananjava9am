6fe117818bdcbdc1115c4d5275829614e7550a02
Smanan2000 <soni.manan3150@gmail.com>
2022-09-11 15:16:12 +0530

    commit

--- java topic/src/filehandling/serialization.java
+++ java topic/src/filehandling/serialization.java
@@ -29,16 +29,24 @@ public class serialization
 	public static void main(String[] args) throws Exception
 	{
 		Employee e = new Employee(1,"manan",10000);
+		Employee e3 = new Employee(2,"manan",20000);
+		Employee e5 = new Employee(3,"manan",40000);
 		FileOutputStream fos = new FileOutputStream("ser1.txt");
 		ObjectOutputStream oos = new ObjectOutputStream(fos);
 		oos.writeObject(e);
+		oos.writeObject(e3);
+		oos.writeObject(e5);
 		oos.flush();
 		oos.close();
 		
 		FileInputStream fis = new FileInputStream("ser1.txt");
 		ObjectInputStream ois = new ObjectInputStream(fis);
 		Employee e2 = (Employee)ois.readObject();
+		Employee e4 = (Employee)ois.readObject();
+		Employee e6 = (Employee)ois.readObject();
 		System.out.println(e);
+		System.out.println(e3);
+		System.out.println(e5);
 	}
 }
 
